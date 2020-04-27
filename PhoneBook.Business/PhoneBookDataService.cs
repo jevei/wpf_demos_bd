857692278ce1379d4fc349ba690a1d9adbab69f4
Jérémy Veillette <1830942@cshawi.ca>
2020-04-27 15:20:38 -0400

    visuel

---
+++ PhoneBook.Business/PhoneBookDataService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PhoneBook.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ PhoneBook.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PhoneBook.Models
+{
+    public class Class1
+    {
+    }
+}
